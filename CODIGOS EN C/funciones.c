46db541cfeaf7a79f0a8b9926678a8c9c9cf0b46
872943 <872943@unizar.es>
2025-04-17 16:17:32 +0200

    unpocodel4

--- CODIGOS EN C/funciones.c
+++ CODIGOS EN C/funciones.c
@@ -234,7 +234,7 @@ int obtener_siguiente_indice(const char* carpeta) {
 }
 
 void muchas_simulaciones_ER(int N_sim, int N_pasos, double dt, double K, double betta, int number_name) {
-    char* direccion_input = "C:\\Users\\USUARIO\\Downloads\\CAOS\\CaOtIcOs\\ARCHIVOS_REDES\\ER\\ER";
+    char* direccion_input = "C:\\Users\\HP\\Desktop\\FISICA\3 (2024-2025)\\segundo cuatri\\caos\\trabajo\\CaOtIcOs\\ARCHIVOS_REDES\\ER";
     char* carpeta_output = "C:\\Users\\USUARIO\\Downloads\\CAOS\\CaOtIcOs\\Resultados (Parte 0)\\Evolucion temporal (promedio y desvest)\\ER";
 
     int indice_salida = obtener_siguiente_indice(carpeta_output);
---
+++ CODIGOS EN C/main(Parte 4).c
@@ -0,0 +1,31 @@
+#include "funciones.h"
+
+
+int main() {
+//   srand(time(NULL)); // Inicializa aleatoriedad
+//   int N_sim=100;
+//   int N_pasos=280;
+//   double dt=0.1;
+//   double betta=0.1;
+//   int number_name=99;
+
+//   muchas_simulaciones_ER(N_sim, N_pasos, dt,K, betta, number_name);
+
+
+
+    double K=10;         // a partir de ahora fijamos K=10
+    double betta_max=1.0
+    double betta= 0
+    double N, delta_betta
+    N=100
+    delta_betta=(betta_max -betta)/N
+    //empezamos con B=0 y realizamos los caluculos, aumentamos la betta un
+    // delta_betta y reprtimos el proceso, y asi sucesivamente hasta B=1.
+
+    //queremos ver cuál es la fracción de estados en los que obtenemos polarización.
+    //Para ello, vamos a simular 200 veces para cada valor de betta para cada <k>.
+
+    int N_sim=200
+
+
+}
