486e0424c68e2331fdd0ecf2f57e8c0b45b05e8b
DonIgnacioHerraiz <874462@unizar.es>
2025-04-27 12:48:32 +0200

    RESULTADOSSSSS (POR FIN)

--- CODIGOS EN C/main(Parte 0).c
+++ CODIGOS EN C/main(Parte 0).c
@@ -6,19 +6,21 @@ int main() {
     srand(time(NULL)); // Inicializa aleatoriedad
     double dt=0.1;
     int N_pasos=50;
-    double K=1.0;
-    int N_redes=5;
+    double K=-1;
+    int N_redes=100;
     int rede_ini=0;
+    double K_final=20;
 
-    double betta_actual=-0.1;
     double betta_final=2;
     double delta_betta=0.1;
-
+    while(K<K_final){
+    K=K+1;
+    double betta_actual=-0.1;
     while(betta_actual<betta_final){
         betta_actual=betta_actual+delta_betta;
-        frac_polarizado(N_redes,rede_ini,K,betta_actual,N_pasos,dt);
-        calcular_fraccion_polarizados(K,betta_actual, N_redes, "RESULTADOS.txt");
+        calcular_fraccion_polarizados(K,betta_actual, N_redes, "Resultados.txt");
     }
+}
   
     /*
            evolucion_persona_a_persona(
